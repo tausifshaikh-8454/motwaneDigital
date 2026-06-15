027dcd9201fe45562d99600faf1eeb12fc37c5c3
tausifShaikh <tausif8454@gmail.com>
2026-06-15 18:11:42 +0530

    Navigation menu added using shade cn

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+
 
 export default function Head() {
   return (
-    <header className='bg-green-900 py-5 px-10'>
-
+    <header className="bg-green-900 py-5 px-10">
+      <div className="">
+        <div className="">
+          <img src="" alt="" />
+        </div>
+        <div className="">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <Link href={`/`}>TXIQ</Link>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div className=""></div>
+      </div>
     </header>
-  )
+  );
 }
