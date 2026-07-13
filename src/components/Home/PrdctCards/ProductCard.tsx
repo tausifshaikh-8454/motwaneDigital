f3fcedae286a98862bc78acb92340364c4e77ac4
tausifShaikh <tausif8454@gmail.com>
2026-07-13 18:04:37 +0530

    service Card transition pending

--- src/components/Home/PrdctCards/ProductCard.tsx
+++ src/components/Home/PrdctCards/ProductCard.tsx
@@ -1,22 +1,40 @@
-import Link from 'next/link'
-import Card1 from '../../../assets/cardImages/TMSiq.webp'
-import { ArrowRight } from 'lucide-react'
+import Link from "next/link";
+import Card1 from "../../../assets/cardImages/TMSiq.webp";
+import { ArrowRight } from "lucide-react";
+import "./prodCard.css";
 
 export default function ProductCard() {
   return (
-    <div>
-        <p className="">Our offerings</p>
-        <h2 className="">Optimizing Performance With Our Cutting-Edge Solutions</h2>
-        <div className="">
-            <div className="">
-                <img src={Card1.src} alt=""/>
-                <div className="infoDesc">
-                    <h5 className="">Transformer Monitoring System</h5>
-                    <p className="">Empowering predictive maintenance of your transformer with online monitoring.</p>
-                    <Link href={'/'}>Readmore<ArrowRight/> </Link>
-                </div>
-            </div>
+    <div className="flex flex-col justify-center items-center px-5 py-20">
+      <p className="font-poppins font-semibold text-[24px]/[36px] text-[#0D6CB3] uppercase pb-2.5">
+        Our offerings
+      </p>
+      <h2 className="font-poppins font-bold text-[40px]/[46px] text-[#000]">
+        Optimizing Performance With Our Cutting-Edge Solutions
+      </h2>
+      <div className="flex flex-col py-10">
+        <div className="absolute h-150 w-100"></div>
+        <div
+          className="mainCard relative h-125 w-100 bg-no-repeat bg-cover flex flex-col justify-end items-start z-8"
+          style={{ backgroundImage: `url(${Card1.src})` }}
+        >
+          {/* <img src={Card1.src} alt=""/> */}
+          <div className="infoDesc w-full h-fit bg-[#0005] backdrop-blur-[2px] px-3 py-5 translate-y-27 transition-transform transition-timing-funtion:ease-in duration-300">
+            <h5 className="font-poppins font-bold text-[22px]/[32px] text-white">
+              Transformer Monitoring System
+            </h5>
+          </div>
+          <div className="serviceLink hidden w-full translate-y-27 h-fit bg-[#0005] backdrop-blur-[2px] px-3 py-5 transition-transform transition-timing-funtion:ease-in duration-300">
+            <p className="font-poppins font-medium text-[18px]/[32px] text-white">
+              Empowering predictive maintenance of your transformer with online
+              monitoring.
+            </p>
+            <Link className="font-poppins font-light text[16px]/[32px] text-white" href={"/"}>
+              Readmore<ArrowRight/>
+            </Link>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
